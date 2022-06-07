bff50983a2b937e047025dab90d96c014a637435
Samir Prakash <Samir.Prakash@gmail.com>
2022-06-06 21:56:04 -0500

    handle status and password

--- users/domain/users/dao.go
+++ users/domain/users/dao.go
@@ -8,10 +8,6 @@ import (
 	"github.com/samirprakash/go-bookstore/users/utils/errors"
 )
 
-const (
-	StatusActive = "active"
-)
-
 // Save saves a new user to the database
 func (user *User) Save() *errors.REST {
 	insertUserQuery := `INSERT INTO users (first_name, last_name, email, created, status, password) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;`
@@ -25,9 +21,9 @@ func (user *User) Save() *errors.REST {
 
 // Get returns a user from the database
 func (user *User) Get() *errors.REST {
-	getUserQuery := `SELECT id, first_name, last_name, email, created, status FROM users WHERE id = $1;`
+	getUserQuery := `SELECT id, first_name, last_name, email, created, status, password FROM users WHERE id = $1;`
 	r := users.DB.QueryRow(context.Background(), getUserQuery, user.ID)
-	err := r.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.Created, &user.Status)
+	err := r.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.Created, &user.Status, &user.Password)
 	if err != nil {
 		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.ID))
 	}
@@ -57,7 +53,7 @@ func (user *User) Delete() *errors.REST {
 // FindByStatus returns a list of users by their status
 func (user *User) FindByStatus(status string) ([]User, *errors.REST) {
 	var uu []User
-	getUsersQuery := `SELECT id, first_name, last_name, email, created FROM users WHERE status = $1;`
+	getUsersQuery := `SELECT id, first_name, last_name, email, created, status, password FROM users WHERE status = $1;`
 	rows, err := users.DB.Query(context.Background(), getUsersQuery, status)
 	if err != nil {
 		return nil, errors.NewInternalServerError(fmt.Sprintf("error getting users: %s", err.Error()))
@@ -66,7 +62,7 @@ func (user *User) FindByStatus(status string) ([]User, *errors.REST) {
 
 	for rows.Next() {
 		var u User
-		err := rows.Scan(&u.ID, &u.FirstName, &u.LastName, &u.Email, &u.Created)
+		err := rows.Scan(&u.ID, &u.FirstName, &u.LastName, &u.Email, &u.Created, &u.Status, &u.Password)
 		if err != nil {
 			return nil, errors.NewInternalServerError(fmt.Sprintf("error getting users: %s", err.Error()))
 		}
--- users/domain/users/dto.go
+++ users/domain/users/dto.go
@@ -6,6 +6,10 @@ import (
 	"github.com/samirprakash/go-bookstore/users/utils/errors"
 )
 
+const (
+	StatusActive = "active"
+)
+
 // User is a struct that represents a user
 type User struct {
 	ID        int64  `json:"id"`
@@ -14,17 +18,21 @@ type User struct {
 	Email     string `json:"email"`
 	Created   string `json:"created"`
 	Status    string `json:"status"`
-	Password  string `json:"-"`
+	Password  string `json:"password"`
 }
+
+type Users []User
 
 // Validate validates the user
 func (user *User) Validate() *errors.REST {
 	user.FirstName = strings.TrimSpace(user.FirstName)
 	user.LastName = strings.TrimSpace(user.LastName)
+
 	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
 	if user.Email == "" {
 		return errors.NewBadRequestError("invalid email address")
 	}
+
 	user.Password = strings.TrimSpace(user.Password)
 	if user.Password == "" {
 		return errors.NewBadRequestError("invalid password")
