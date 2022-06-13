0d0041f1ec507435c3aed16c7fec861741d09c76
Samir Prakash <Samir.Prakash@gmail.com>
2022-06-13 12:07:23 -0500

    define domain for token

---
+++ oauth/domain/token/token.go
@@ -0,0 +1,22 @@
+package token
+
+import "time"
+
+const expirationTime = time.Hour * 24
+
+type Token struct {
+	Token     string `json:"token"`
+	UserID    int64  `json:"user_id"`
+	ClientID  int64  `json:"client_id"`
+	ExpiresAt int64  `json:"expires_at"`
+}
+
+func NewToken() *Token {
+	return &Token{
+		ExpiresAt: time.Now().UTC().Add(expirationTime).Unix(),
+	}
+}
+
+func (t Token) isExpired() bool {
+	return time.Now().UTC().Unix() > t.ExpiresAt
+}
