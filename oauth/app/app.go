79a5a651ad8dcc3ffd130af896b24c99758c6ebf
Samir Prakash <Samir.Prakash@gmail.com>
2022-06-30 23:06:08 -0500

    create token API

--- oauth/app/app.go
+++ oauth/app/app.go
@@ -14,6 +14,7 @@ func Start() {
 	h := http.NewTokenHandler(s)
 
 	router.GET("/oauth/token/:id", h.GetByID)
+	router.POST("/oauth/token", h.Create)
 
 	router.Run(":8080")
 }
--- oauth/http/token.go
+++ oauth/http/token.go
@@ -5,10 +5,12 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/samirprakash/go-bookstore/oauth/domain/token"
+	"github.com/samirprakash/go-bookstore/oauth/utils/errors"
 )
 
 type TokenHandler interface {
 	GetByID(*gin.Context)
+	Create(*gin.Context)
 }
 
 type tokenHandler struct {
@@ -25,6 +27,38 @@ func (h *tokenHandler) GetByID(c *gin.Context) {
 	id := c.Param("id")
 	t, err := h.service.GetByID(id)
 	if err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, t)
+}
+
+func (h *tokenHandler) Create(c *gin.Context) {
+	var t token.Token
+	if err := c.ShouldBindJSON(&t); err != nil {
+		rErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(rErr.Status, rErr)
+		return
+	}
+
+	if err := h.service.Create(t); err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, t)
+}
+
+func (h *tokenHandler) UpdateExpiration(c *gin.Context) {
+	var t token.Token
+	if err := c.ShouldBindJSON(&t); err != nil {
+		rErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(rErr.Status, rErr)
+		return
+	}
+
+	if err := h.service.UpdateExpiration(t); err != nil {
 		c.JSON(err.Status, err)
 		return
 	}
