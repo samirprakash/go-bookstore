34f8ed32ffd061fd0ebd515b41b4fefa988bbd74
Samir Prakash <Samir.Prakash@gmail.com>
2022-06-30 23:05:51 -0500

    init cassandra cluster

--- oauth/clients/cassandra/cassandra.go
+++ oauth/clients/cassandra/cassandra.go
@@ -6,7 +6,7 @@ import (
 
 var cluster *gocql.ClusterConfig
 
-func init() {
+func New() {
 	// connect to the cassandra cluster
 	cluster = gocql.NewCluster("127.0.0.1:9042")
 	cluster.Keyspace = "oauth"
--- oauth/main.go
+++ oauth/main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"log"
-
 	"github.com/samirprakash/go-bookstore/oauth/app"
 	"github.com/samirprakash/go-bookstore/oauth/clients/cassandra"
 )
 
 func main() {
-	// check connectivity to the cassandra cluster
-	// close the session after the check
-	session, err := cassandra.GetSession()
-	if err != nil {
-		log.Fatal(err)
-	}
-	session.Close()
+	// initialize the cassandra cluster
+	cassandra.New()
 
 	// start the application
 	app.Start()
