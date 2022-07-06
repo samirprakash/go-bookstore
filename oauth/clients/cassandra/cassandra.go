4a6e62da5c7fffe9450e522f0f962d67dfd993a4
Samir Prakash <Samir.Prakash@gmail.com>
2022-07-05 20:37:04 -0500

    use concurrent session

--- oauth/clients/cassandra/cassandra.go
+++ oauth/clients/cassandra/cassandra.go
@@ -4,15 +4,20 @@ import (
 	"github.com/gocql/gocql"
 )
 
-var cluster *gocql.ClusterConfig
+var session *gocql.Session
 
 func New() {
 	// connect to the cassandra cluster
-	cluster = gocql.NewCluster("127.0.0.1:9042")
+	cluster := gocql.NewCluster("127.0.0.1:9042")
 	cluster.Keyspace = "oauth"
 	cluster.Consistency = gocql.Quorum
+
+	var err error
+	if session, err = cluster.CreateSession(); err != nil {
+		panic(err)
+	}
 }
 
-func GetSession() (*gocql.Session, error) {
-	return cluster.CreateSession()
+func GetSession() *gocql.Session {
+	return session
 }
--- oauth/repository/db/db.go
+++ oauth/repository/db/db.go
@@ -22,31 +22,17 @@ func NewRepository() Repository {
 }
 
 func (r *repository) Create(t token.Token) *errors.REST {
-	session, err := cassandra.GetSession()
-	if err != nil {
-		return errors.NewInternalServerError("database error")
-	}
-	defer session.Close()
-
-	ctx := context.Background()
 	queryCreate := "INSERT INTO oauth.tokens (id, user_id, client_id, expires_at) VALUES (?, ?, ?, ?);"
-	if err := session.Query(queryCreate, t.ID, t.UserID, t.ClientID, t.ExpiresAt).WithContext(ctx).Exec(); err != nil {
+	if err := cassandra.GetSession().Query(queryCreate, t.ID, t.UserID, t.ClientID, t.ExpiresAt).WithContext(context.Background()).Exec(); err != nil {
 		return errors.NewInternalServerError("database error")
 	}
 	return nil
 }
 
 func (r *repository) GetByID(id string) (*token.Token, *errors.REST) {
-	session, err := cassandra.GetSession()
-	if err != nil {
-		return nil, errors.NewInternalServerError("database error")
-	}
-	defer session.Close()
-
-	ctx := context.Background()
 	token := token.Token{}
 	getTokenByIDQuery := "SELECT id, user_id, client_id, expires_at FROM oauth.tokens WHERE id = ?;"
-	if err := session.Query(getTokenByIDQuery, id).WithContext(ctx).Scan(&token.ID, &token.UserID, &token.ClientID, &token.ExpiresAt); err != nil {
+	if err := cassandra.GetSession().Query(getTokenByIDQuery, id).WithContext(context.Background()).Scan(&token.ID, &token.UserID, &token.ClientID, &token.ExpiresAt); err != nil {
 		if err == gocql.ErrNotFound {
 			return nil, errors.NewNotFoundError("token not found in database")
 		}
@@ -57,15 +43,8 @@ func (r *repository) GetByID(id string) (*token.Token, *errors.REST) {
 }
 
 func (r *repository) UpdateExpiration(t token.Token) *errors.REST {
-	session, err := cassandra.GetSession()
-	if err != nil {
-		return errors.NewInternalServerError("database error")
-	}
-	defer session.Close()
-
-	ctx := context.Background()
 	queryUpdate := "UPDATE oauth.tokens SET expires_at = ? WHERE id = ?;"
-	if err := session.Query(queryUpdate, t.ExpiresAt, t.ID).WithContext(ctx).Exec(); err != nil {
+	if err := cassandra.GetSession().Query(queryUpdate, t.ExpiresAt, t.ID).WithContext(context.Background()).Exec(); err != nil {
 		return errors.NewInternalServerError("database error")
 	}
 	return nil
