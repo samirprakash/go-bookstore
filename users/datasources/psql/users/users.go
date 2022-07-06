91087bf4ed58eca4d959561714fa7aff5754d62d
Samir Prakash <Samir.Prakash@gmail.com>
2022-07-05 21:17:07 -0500

    update logging

--- users/datasources/psql/users/users.go
+++ users/datasources/psql/users/users.go
@@ -2,9 +2,9 @@ package users
 
 import (
 	"context"
+	"log"
 
 	"github.com/jackc/pgx/v4"
-	"github.com/samirprakash/go-bookstore/users/logger"
 )
 
 var (
@@ -15,6 +15,6 @@ func init() {
 	var err error
 	DB, err = pgx.Connect(context.Background(), "postgres://root:secret@localhost:5432/bookstore_users?sslmode=disable")
 	if err != nil {
-		logger.Error("error when connecting to database: %s", err)
+		log.Printf("error when connecting to database: %s\n", err)
 	}
 }
